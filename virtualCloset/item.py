251f67eee31595254282a6fd1d82ffb14abe97ac
MarjanBaghdadi <35376484+MarjanBaghdadi@users.noreply.github.com>
2022-03-08 12:30:26 +0100

    Add files via upload

---
+++ virtualCloset/item.py
@@ -0,0 +1,14 @@
+class Item:
+    
+    def __init__ (self, itemName, itemcColor):
+        self.itemName = itemName
+        self.itemcColor = itemcColor
+
+        
+    """def (self, x):
+        if self.x
+            return True
+        else:
+            return False
+    """
+            
